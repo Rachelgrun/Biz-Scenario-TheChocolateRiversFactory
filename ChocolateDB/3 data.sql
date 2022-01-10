a2c43dfe179f25c9e76309013004d1c4cefae822
bgoldfischer <bgoldfischer@cpucodeschool.com>
2022-01-10 17:26:43 +0000

    partial work

---
+++ ChocolateDB/3 data.sql
@@ -0,0 +1,42 @@
+insert Chocolate(ChocolateType, ChocolateFlavor, ChocolateShape, Country, YearOnMarket, WeightOfChocolate, DateOfExpiration, DateSold)
+select 
+    ChocolateType = 
+        case 
+            when m.Medal = 'gold'  then 'Solid Block'
+            when m.medal = 'silver' then 'Chocolate Trouffles'
+            when m.Medal = 'Bronze' then 'Chocolate Candy Bar'
+        end,
+    ChocolateFlavor = 
+        case 
+            when m.SportSubcategory like '%Women%' or m.SportSubcategory like '%ladies%' then 'Caramalized white chocolate'
+            when m.SportSubcategory like 'Men%' then 'Milk Chocolate Hazelnut Espresso'
+            when m.SportSubcategory like '%[0-9]%' then 'Dark 54 Chocolate Rasberry Pomegranate'
+            when 
+                m.SportSubcategory not like '%Women%' and 
+                m.SportSubcategory not like '%ladies%' and  
+                m.SportSubcategory  not like 'Men%' and  
+                m.SportSubcategory not like '%[0-9]%' and 
+                m.SportSubcategory like '% %' 
+            then 'Dark salted caramel'
+            else 'milk creme & Dark Creme'
+        end,
+    ChocolateShape =
+        case 
+            when m.OlympicYear - m.YearBorn between 14 and 16 then 'heart'
+            when m.OlympicYear - m.YearBorn between 17 and 27 then 'oval'
+            when m.OlympicYear - m.YearBorn between 29 and 41 then 'square'
+            when m.OlympicYear - m.YearBorn between 48 and 58 then 'oracle'
+            when m.OlympicYear - m.YearBorn between 59 and 60 then 'ripple'
+            else 'thimble'
+        end,
+    Country = m.Country,
+    YearOnMarket = m.YearBorn,
+    WeightOfChocolate = 
+        (m.OlympicYear - m.YearBorn) / 10,
+    DateOfExpiration = '01/02/2025',
+        --concat(datepart(year, m.OlympicYear) + 5, '-', '0', datepart(month, m.OlympicYear - m.YearBorn / 6), '-', datepart(day, m.OlympicYear - m.YearBorn / 4)),
+    DateSold = null
+    --concat(datepart(year, m.OlympicYear), '-', '0', datepart(month, m.OlympicYear - m.YearBorn / 6), '-', datepart(day, m.OlympicYear - m.YearBorn / 4))
+from RecordKeeperDB.dbo.Medalist m 
+
+select * from Chocolate p 
---
+++ ChocolateDB/4 reports.sql
@@ -0,0 +1,22 @@
+/*Reports:
+1)Inorder to see which flavor is most popular, can you show me how long has each flavor been 
+sitting on the shelf before it was sold (per chocolate flavor and chocolateType)? overall which flavor
+was on the shelf the longest? which the shortest?
+*/
+
+
+
+
+
+/*
+2)Can you print out a list as of January first 1917 which chocolate were put on the shelf but not yet sold.They got sold within the next 17 years.
+show the pieces in this order: Chocolate Name,yearPutOnShelf,DateSold,#Yearsold(the year it got sold after this point)
+*/
+
+
+
+/*
+3) Mix together some flavors by subtracting 2 ingredients from 3-5 and adding 2 from 1 and 2 
+what number flavor comes out? present the Recipe as the chocolate type description colon the chocolate flavor description colon the chocolate flavor numbernand then
+which operation it will get iehther plus 2 or minus 2 based on its flavor number i.e Chocolate Candy Bar:milk creme & Dark Creme: 5-2
+*/
